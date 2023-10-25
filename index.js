35a23567efb0447a380001c15525d03efc009cda
Asrafunnessa <ayeshapriya0@gmail.com>
2023-10-25 10:07:42 +0600

    product post and get with brandname

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5500;
+
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xzmsh8k.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 
@@ -26,6 +27,53 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productsCollection = client.db('productsDB').collection('products');
+    const cartsCollection = client.db('productsDB').collection('carts');
+
+    app.get('/products', async(req, res) =>{
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+ 
+    app.get('/products/:name', async(req, res) =>{
+      const name = req.params.name;
+      console.log(name);
+      const query = {brandName: name};
+      const cursor = productsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/products/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.get('/details', async(req, res) =>{
+      const cursor = cartsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/details', async(req, res) => {
+      const newCart = req.body;
+      console.log(newCart);
+      const result = await cartsCollection.insertOne(newCart);
+      res.send(result);
+    })
+
+
+    app.post('/products', async(req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
